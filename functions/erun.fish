37bada776ce51f483c8f0e7ff0ca1fd650297f0b
Shion Shimizu <shimizu.shion819@gmail.com>
2024-05-30 00:39:12 +0900

    Updates in functions
    - improving `erun` and `etest`
    - adding `edo` and `elint`

--- functions/erun.fish
+++ functions/erun.fish
@@ -1,4 +1,12 @@
 function erun -d "efficient easifem run"
+    if set -q EASIFEM_PYTHON_CLI
+        _erun_python $argv
+    else
+        _erun_go $argv
+    end
+end
+
+function _erun_python
     set -l currentPath ( pwd )
 
     argparse \
@@ -10,20 +18,18 @@ function erun -d "efficient easifem run"
 
     set -l cands "easifemBase\neasifemClasses\neasifemAcoustic\neasifemElasticity"
 
-    if set -q EASIFEM_PYTHON_CLI
-        if set -ql _flag_acoustic
-            set eflag easifemAcoustic
-        else if set -ql _flag_elasticity
-            set eflag easifemElasticity
-        else if set -ql _flag_classes
-            set eflag easifemClasses
-        else if set -ql _flag_base
-            set eflag easifemBase
-        else if set -ql _flag_modname
-            set eflag $_flag_modname
-        else
-            set eflag (echo -e $cands | fzf )
-        end
+    if set -ql _flag_acoustic
+        set eflag easifemAcoustic
+    else if set -ql _flag_elasticity
+        set eflag easifemElasticity
+    else if set -ql _flag_classes
+        set eflag easifemClasses
+    else if set -ql _flag_base
+        set eflag easifemBase
+    else if set -ql _flag_modname
+        set eflag $_flag_modname
+    else
+        set eflag (echo -e $cands | fzf -m )
     end
 
     if set -ql _flag_quiet
@@ -38,22 +44,47 @@ function erun -d "efficient easifem run"
     else
         if set -ql _flag_directory
             set dirname (fd --type d | fzf )
-            set filename (find $dirname  -name "*.[fF]*" -o -name "*.md" -type f | fzf )
+            set filename (fd $dirname --type f -e md -e F90 -e f90 | fzf )
         else
-            set filename (find . -name "*.[fF]*" -o -name "*.md" -type f | fzf )
+            set filename (fd .  --type f -e md -e F90 -e f90 | fzf )
         end
     end
 
-    if set -q EASIFEM_PYTHON_CLI
-        if set -ql _flag_smartout
-            easifem $run -e $eflag -f $filename >tmp_erun
-            _easifem_output_reshape tmp_erun
-            rm -f tmp_erun
-        else
-            easifem $run -e $eflag -f $filename
-        end
+    if set -ql _flag_smartout
+        easifem $run -e $eflag -f $filename >tmp_erun
+        _easifem_output_reshape tmp_erun
+        rm -f tmp_erun
     else
-        easifem $run $filename
+        easifem $run -e $eflag -f $filename
     end
+
+    builtin cd $currentPath
+end
+
+function _erun_go
+    set -l currentPath ( pwd )
+
+    argparse q/quiet d/directory -- $argv
+    or return 1
+
+    if set -ql _flag_quiet
+        set run run -q
+    else
+        set run run
+    end
+
+    if count $argv >/dev/null
+        set filename $argv
+    else
+        if set -ql _flag_directory
+            set dirname (fd --type d | fzf )
+            set filename (fd $dirname --type f -e md -e F90 -e f90 | fzf -m )
+        else
+            set filename (fd . --type f -e md -e F90 -e f90 | fzf -m )
+        end
+    end
+
+    easifem $run $filename
+
     builtin cd $currentPath
 end
--- functions/etest.fish
+++ functions/etest.fish
@@ -1,4 +1,12 @@
 function etest -d "efficient easifem test"
+    if set -q EASIFEM_PYTHON_CLI
+        _etest_python $argv
+    else
+        _etest_go $argv
+    end
+end
+
+function _etest_python
     set -l currentPath ( pwd )
 
     argparse \
@@ -10,20 +18,18 @@ function etest -d "efficient easifem test"
 
     set -l cands "easifemBase\neasifemClasses\neasifemAcoustic\neasifemElasticity"
 
-    if set -q EASIFEM_PYTHON_CLI
-        if set -ql _flag_acoustic
-            set eflag easifemAcoustic
-        else if set -ql _flag_elasticity
-            set eflag easifemElasticity
-        else if set -ql _flag_classes
-            set eflag easifemClasses
-        else if set -ql _flag_base
-            set eflag easifemBase
-        else if set -ql _flag_modname
-            set eflag $_flag_modname
-        else
-            set eflag (echo -e $cands | fzf )
-        end
+    if set -ql _flag_acoustic
+        set eflag easifemAcoustic
+    else if set -ql _flag_elasticity
+        set eflag easifemElasticity
+    else if set -ql _flag_classes
+        set eflag easifemClasses
+    else if set -ql _flag_base
+        set eflag easifemBase
+    else if set -ql _flag_modname
+        set eflag $_flag_modname
+    else
+        set eflag (echo -e $cands | fzf -m )
     end
 
     if set -ql _flag_quiet
@@ -31,23 +37,39 @@ function etest -d "efficient easifem test"
     else
         set run run
     end
-    # echo $eflag
 
     builtin cd $docs
     set -l test_dir $docs/(fd --type d | fzf)
     builtin cd $test_dir
 
-    if set -q EASIFEM_PYTHON_CLI
-        if set -ql _flag_smartout
-            easifem $run -e $eflag -f (fd -I --type f -e md -e F90 -e f90  | fzf ) >tmp_etest
-            _easifem_output_reshape tmp_etest
-            rm -f tmp_etest
-        else
-            easifem $run -e $eflag -f (fd -I --type f -e md -e F90 -e f90  | fzf )
-        end
+    if set -ql _flag_smartout
+        easifem $run -e $eflag -f (fd -I --type f -e md -e F90 -e f90  | fzf ) >tmp_etest
+        _easifem_output_reshape tmp_etest
+        rm -f tmp_etest
     else
-        easifem $run (fd -I --type f -e md -e F90 -e f90  | fzf )
+        easifem $run -e $eflag -f (fd -I --type f -e md -e F90 -e f90  | fzf )
     end
+
+    builtin cd $currentPath
+end
+
+function _etest_go
+    set -l currentPath ( pwd )
+
+    argparse q/quiet -- $argv
+    or return 1
+
+    if set -ql _flag_quiet
+        set run run -q
+    else
+        set run run
+    end
+
+    builtin cd $docs
+    set -l test_dir $docs/(fd --type d | fzf)
+    builtin cd $test_dir
+
+    easifem $run (fd -I --type f -e md -e F90 -e f90  | fzf -m )
 
     builtin cd $currentPath
 end
